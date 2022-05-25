d2809946a00f713337dd9c59c3ad9b20df1def6c
Mauricio Costa <malesupremacy91@hotmail.com>
2022-05-25 19:26:38 -0300

    graficoacoespy

--- graficoacoes.py
+++ graficoacoes.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 cotacao_bovespa=web.DataReader("MGLU3.SA", data_source="yahoo", start="01/01/2020", end="01/01/2022")
 display(cotacao_bovespa)
-cotacao_bovespa["Adj Close"].plot(figsize=(15, 10))
+cotacao_bovespa["Adj Close"].plot(figsize=(10, 5))
 plt.show()
 
 
